f096cbfba2a21dc8b9f80364ebc4276119871bbd
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-14 11:50:47 -0800

    Getting back JSON data on request

--- weatherApp.py
+++ weatherApp.py
@@ -1,10 +1,25 @@
 import tkinter as tk
+import requests
 
 HEIGHT =500
 WIDTH =600
 
 def test_function(entry):
     print("This is the entry: ", entry)
+
+#cfe3b6ec5054916590a018157fe473ff
+#http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    
+
+
+def get_weather(city):
+        weather_key = 'cfe3b6ec5054916590a018157fe473ff'
+        url='http://api.openweathermap.org/data/2.5/forecast'
+        params={'APPID': weather_key, 'q': city, 'units': 'metric'}
+        response = requests.get(url, params=params)
+        print(response.json())
+
+
 
 
 
@@ -27,7 +42,7 @@ entry.place(relwidth=0.65, relheight=1)
 
 
 
-button = tk.Button(frame, text="Get Weather", font='20', bg="gray", fg="blue", command=lambda:test_function(entry.get()))
+button = tk.Button(frame, text="Get Weather", font='20', bg="gray", fg="blue", command=lambda:get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg="#2167a1", bd=10)
