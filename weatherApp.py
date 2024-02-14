0748ed4f03d9f6a2d947773f9d62822c3c4d8512
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-14 11:09:25 -0800

    Connected the button to the entry

--- weatherApp.py
+++ weatherApp.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 
-HEIGHT =700
-WIDTH = 800
+HEIGHT =500
+WIDTH =600
+
+def test_function(entry):
+    print("This is the entry: ", entry)
+
+
 
 root = tk.Tk()
 
@@ -9,7 +14,12 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg='#80c1ff', bd=5)
+
+
+
+
+
+frame = tk.Frame(root, bg='#D2AF34', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font=40)
@@ -17,9 +27,14 @@ entry.place(relwidth=0.65, relheight=1)
 
 
 
-button = tk.Button(frame, text="Test Button", font='20', bg="gray", fg="blue")
-
+button = tk.Button(frame, text="Get Weather", font='20', bg="gray", fg="blue", command=lambda:test_function(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg="#2167a1", bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
 
 
 root.mainloop()
